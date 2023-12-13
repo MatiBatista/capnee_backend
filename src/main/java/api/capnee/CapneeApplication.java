2476e3abc37923c18740ba6e7ccb6914e7170605
MatiBatista <matiasomarbatista1@gmail.com>
2023-12-13 13:08:31 -0300

    Configuracion inicial de proyecto Spring Boot

---
+++ src/main/java/api/capnee/CapneeApplication.java
@@ -0,0 +1,13 @@
+package api.capnee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapneeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapneeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/capnee/CapneeApplicationTests.java
@@ -0,0 +1,13 @@
+package api.capnee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapneeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
